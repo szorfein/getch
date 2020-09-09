9dab4a959b6e7e5b5d66397e8b48996b67eb2aad
szorfein <szorfein@protonmail.com>
2020-09-09 18:10:05 +0200

    install ruby if no found

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,19 +3,54 @@
 set -o errexit -o nounset
 
 DIR=/tmp/getch-master
+PATH=${PATH}:/tmp/ruby/bin
 
-if hash ruby 2>/dev/null ; then
-  echo "Ruby is found"
-else
-  echo "Install ruby"
-fi
+compile_ruby() {
+  PN=ruby
+  PV=2.7
+  [ -f /tmp/$PN_$PV.tar.gz ] || curl -s -L -o /tmp/$PN_$PV.tar.gz https://cache.ruby-lang.org/pub/ruby/snapshot/snapshot-$PN_$PV.tar.gz
+  [ -d /tmp/$PN_$PV ] || {
+    cd /tmp
+    tar xvf $PN_$PV.tar.gz
+    cd snapshot-$PN_$PV
+    ./configure --prefix=/tmp/$PN
+    make
+    make install
+  }
+}
 
-cd /tmp
-[ -f ./getch.tar.gz ] && rm ./getch.tar.gz
-[ -d ./getch-master ] && rm -rf ./getch-master
+search_ruby() {
+  if hash ruby 2>/dev/null ; then
+    echo "Ruby $(ruby -v | awk '{print $2}') found"
+  else
+    echo "Install ruby"
+    if hash pacman 2>/dev/null ; then
+      pacman -Syy
+      pacman -S --needed ruby
+    elif hash emerge 2>/dev/null ; then
+      emerge -av dev-lang/ruby
+    elif hash apt-get 2>/dev/null ; then
+      apt-get install ruby
+    else
+      compile_ruby
+    fi
+  fi
+}
 
-curl -s -L -o getch.tar.gz https://github.com/szorfein/getch/archive/master.tar.gz
-tar xvf getch.tar.gz
+get_getch() {
+  cd /tmp
+  [ -f ./getch.tar.gz ] && rm ./getch.tar.gz
+  [ -d ./getch-master ] && rm -rf ./getch-master
 
-cd $DIR \
-  && ruby -I lib bin/getch
+  curl -s -L -o getch.tar.gz https://github.com/szorfein/getch/archive/master.tar.gz
+  tar xzf getch.tar.gz
+}
+
+main() {
+  get_getch
+  search_ruby
+  cd $DIR \
+    && ruby -I lib bin/getch
+}
+
+main "$@"
