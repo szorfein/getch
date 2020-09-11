f75c3a73d48e1bd3cbb17170b5788d0ed01ceb0c
szorfein <szorfein@protonmail.com>
2020-09-11 11:48:26 +0200

    default options

--- lib/getch.rb
+++ lib/getch.rb
@@ -4,21 +4,29 @@ module Getch
 
   DEFAULT_OPTIONS = {
     language: 'en_US',
-    location: 'Europe/Paris',
-    keyboard: 'fr',
+    location: 'US/Eastern',
+    keyboard: 'us',
     disk: 'sda',
     fs: 'ext4',
     username: ''
   }
 
   def self.main(argv)
-    puts "Starting..."
-    options = Options.new(argv)
-    puts options.zoneinfo
-    puts DEFAULT_OPTIONS[:language]
-    puts DEFAULT_OPTIONS[:location]
-    puts DEFAULT_OPTIONS[:keyboard]
-    puts DEFAULT_OPTIONS[:disk]
-    puts DEFAULT_OPTIONS[:fs]
+    options = Options.new(DEFAULT_OPTIONS, argv)
+    puts "\nBuild Gentoo with the following args:\n"
+    puts "lang: #{options.language}"
+    puts "zoneinfo: #{options.zoneinfo}"
+    puts "keyboard: #{options.keyboard}"
+    puts "disk: #{options.disk}"
+    puts "fs: #{options.fs}"
+    puts "username: #{options.username}"
+    puts
+    print "Continue? (n,y) "
+    case gets.chomp
+    when /^y|^Y/
+      puts "Right"
+    else
+      exit 1
+    end
   end
 end
--- lib/getch/options.rb
+++ lib/getch/options.rb
@@ -4,13 +4,13 @@ module Getch
   class Options
     attr_reader :language, :zoneinfo, :keyboard, :disk, :fs, :username
 
-    def initialize(argv)
-      @language = ""
-      @zoneinfo = ""
-      @keyboard = ""
-      @disk = ""
-      @fs = ""
-      @username = ""
+    def initialize(default, argv)
+      @language = default[:language]
+      @zoneinfo = default[:location]
+      @keyboard = default[:keyboard]
+      @disk = default[:disk]
+      @fs = default[:fs]
+      @username = default[:username]
       parse(argv)
     end
 
@@ -27,7 +27,7 @@ module Getch
         opts.on("-k", "--keyboard KEY", "Default is us") do |key|
           @keyboard = key
         end
-        opts.on("-d", "--disk DISK", "Name for disk (sda,sdb)") do |disk|
+        opts.on("-d", "--disk DISK", "Disk where install Gentoo (sda,sdb)") do |disk|
           @disk = disk
         end
         opts.on("-f", "--format FS", "Default use ext4") do |fs|
@@ -40,15 +40,7 @@ module Getch
           puts opts
           exit
         end
-
-        begin
-          argv = ["-h"] if argv.empty?
-          opts.parse!(argv)
-        rescue OptionParser::ParseError => e
-          STDERR.puts e.message, "\n", opts
-          exit(-1)
-        end
-      end
+      end.parse!
     end
   end
 end
