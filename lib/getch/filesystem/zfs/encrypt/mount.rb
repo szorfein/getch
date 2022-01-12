4301e33f3e0b2fd5cb9027d8f1c39255a14ee763
szorfein <szorfein@protonmail.com>
2022-01-12 09:46:20 -0500

    zfs - import by id

--- lib/getch/filesystem/zfs/encrypt/mount.rb
+++ lib/getch/filesystem/zfs/encrypt/mount.rb
@@ -10,6 +10,7 @@ module Getch
             @mount = Getch::FileSystem::Mount.new
             @state = Getch::States.new
             @log = Getch::Log.new
+            @import = '/dev/disk/by-id'
           end
 
           def run
@@ -17,8 +18,8 @@ module Getch
 
             exec('zpool export -a')
             exec("rm -rf #{MOUNTPOINT}/*")
-            exec("zpool import -N -R #{MOUNTPOINT} #{@pool_name}")
-            exec("zpool import -f -N -R #{MOUNTPOINT} #{@boot_pool_name}") if @dev_boot
+            exec("zpool import -N -d #{@import} -R #{MOUNTPOINT} #{@pool_name}")
+            exec("zpool import -f -N -d #{@import} -R #{MOUNTPOINT} #{@boot_pool_name}") if @dev_boot
             exec('zfs load-key -a')
             mount_root
             mount_boot
