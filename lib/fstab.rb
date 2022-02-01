098d14d547a188407feb2a543ef1c2c36d33d0d0
szorfein <szorfein@protonmail.com>
2022-02-01 11:14:42 -0500

    fstab with uuid

--- lib/fstab.rb
+++ lib/fstab.rb
@@ -1,19 +1,95 @@
+# frozen_string_litteral: true
+
 require 'nito'
+require_relative 'getch/log'
 
 class Fstab
   include NiTo
 
   def initialize(devs, options)
+    @log = Getch::Log.new
     @esp = devs[:esp] ||= nil
     @boot = devs[:boot] ||= nil
     @swap = devs[:swap] ||= nil
     @root = devs[:root] ||= nil
     @home = devs[:home] ||= nil
+    @fs = options[:fs] ||= 'ext4'
     @mountpoint = options[:mountpoint] ||= '/mnt/getch'
+    @conf = "#{@mountpoint}/etc/fstab"
+  end
+
+  def generate
+    @log.info 'Generating fstab...'
+    write_esp
+    write_boot
+    write_swap
+    write_root
+    write_home
+    write_tmp
+    @log.result 'Ok'
+  end
+
+  protected
+
+  def write_esp
+    @esp || return
+
+    uuid = gen_uuid @esp
+    line = "UUID=#{uuid} /efi vfat noauto,rw,relatime 0 0"
+    echo_a @conf, line
+  end
+
+  def write_boot
+    @boot || return
+
+    uuid = gen_uuid @boot
+    line = "UUID=#{uuid} /boot #{@fs} noauto,rw,relatime 0 0"
+    echo_a @conf, line
+  end
+
+  def write_swap
+    @swap || return
+
+    uuid = gen_uuid @swap
+    line = "UUID=#{uuid} swap swap rw,noatime,discard 0 0"
+    echo_a @conf, line
+  end
+
+  def write_root
+    @root || return
+
+    uuid = gen_uuid @root
+    line = "UUID=#{uuid} / #{@fs} rw,relatime 0 1"
+    echo_a @conf, line
+  end
+
+  def write_home
+    @home || return
+
+    uuid = gen_uuid @home
+    line = "UUID=#{uuid} /home #{@fs} noauto,rw,relatime 0 2"
+    echo_a @conf, line
+  end
+
+  def write_tmp
+    systemd? && return
+
+    line = 'tmpfs /tmp tmpfs defaults,nosuid,nodev 0 0'
+    echo_a @conf, line
   end
 
   private
 
-  def gen_uuid
+  def gen_uuid(dev)
+    device = dev.delete_prefix('/dev/')
+    Dir.glob('/dev/disk/by-uuid/*').each do |f|
+      link = File.readlink(f)
+      return f.delete_prefix('/dev/disk/by-uuid/') if link.match(/#{device}$/)
+    end
+    @log.fatal "No uuid found for #{device}"
+  end
+
+  def systemd?
+    Dir.exist? "#{@mountpoint}/etc/systemd"
   end
 end
--- lib/getch/filesystem/ext4/config.rb
+++ lib/getch/filesystem/ext4/config.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'fstab'
+
 module Getch
   module FileSystem
     module Ext4
@@ -12,17 +14,18 @@ module Getch
         protected
 
         def x
-          gen_uuid
           fstab
-          cmdline
+          #gen_uuid
+          #cmdline
         end
 
         private
 
         def fstab
-          file = "#{MOUNTPOINT}/etc/fstab"
-          datas = data_fstab
-          File.write(file, datas.join("\n"))
+          devs = {
+            esp: @dev_esp, swap: @dev_swap, root: @dev_root, home: @dev_home
+          }
+          Fstab.new(devs, OPTIONS).generate
         end
 
         def cmdline
@@ -36,17 +39,6 @@ module Getch
         def gen_uuid
           @partuuid_root = Helpers.partuuid(@dev_root)
           @partuuid_swap = Helpers.partuuid(@dev_swap)
-          @uuid_root = `lsblk -o "UUID" #{@dev_root} | tail -1`.chomp() if @dev_root
-          @uuid_esp = `lsblk -o "UUID" #{@dev_esp} | tail -1`.chomp() if @dev_esp
-          @uuid_home = `lsblk -o "UUID" #{@dev_home} | tail -1`.chomp() if @dev_home
-        end
-
-        def data_fstab
-          esp = @dev_esp ? "UUID=#{@uuid_esp} /efi vfat noauto,noatime 1 2" : ''
-          swap = @dev_swap ? "PARTUUID=#{@partuuid_swap} none swap discard 0 0" : ''
-          root = @dev_root ? "UUID=#{@uuid_root} / ext4 defaults 0 1" : ''
-          home = @dev_home ? "UUID=#{@uuid_home} /home/#{@user} ext4 defaults 0 2" : ''
-          [ esp, swap, root, home ]
         end
       end
     end
