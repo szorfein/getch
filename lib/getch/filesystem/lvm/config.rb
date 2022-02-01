8b1e1bc02ce38ec4b2a02cba64f2acf5cb5e30ef
szorfein <szorfein@protonmail.com>
2022-02-01 15:05:51 -0500

    fstab with Luks

--- lib/getch/filesystem/lvm/config.rb
+++ lib/getch/filesystem/lvm/config.rb
@@ -7,28 +7,14 @@ module Getch
     module Lvm
       class Config < Getch::FileSystem::Lvm::Device
         def fstab
-          devs = { esp: @dev_esp, boot: @dev_boot }
-          Fstab.new(devs, OPTIONS).generate
-
-          file = "#{MOUNTPOINT}/etc/fstab"
-          datas = data_fstab
-          File.write(file, datas.join("\n"))
+          devs = { esp: @dev_esp }
+          Fstab::Lvm.new(devs, OPTIONS).generate
         end
 
         def cmdline
           conf = "#{MOUNTPOINT}/etc/dracut.conf.d/cmdline.conf"
           line = "resume=#{@lv_swap} rd.lvm.vg=#{@vg}"
           File.write conf, "kernel_cmdline=\"#{line}\"\n"
-        end
-
-        private
-
-        def data_fstab
-          swap = "#{@lv_swap} none swap discard 0 0"
-          root = "#{@lv_root} / ext4 defaults 0 1"
-          home = @lv_home ? "#{@lv_home} /home/#{@user} ext4 defaults 0 2" : ''
-
-          [ swap, root, home ]
         end
       end
     end
